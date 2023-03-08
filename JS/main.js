1901fdd02d8f2035c5a6e439e53845aaaaffeec5
fedeiramain <federicoiramain@gmail.com>
2023-03-08 09:31:46 -0300

    func usuario

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,7 @@
 alert("Bienvenidos a Prestamos!!");
 
-let usuario = prompt("Por favor, ingrese su usuario:");
+function inicieSesion () {
+    let usuario = prompt("Por favor, ingrese su usuario:");
 while (usuario == "") {
     usuario = prompt("Por favor, ingrese su usuario:");
 };
@@ -11,6 +12,7 @@ let autoriza = 1234;
 while (contraseña != autoriza) {
     contraseña = prompt("incorrecto, ingrese su clave:");
 };
+};
 
 alert("Prestamos personales");
 
@@ -18,38 +20,39 @@ let ingresos = prompt("Indique sus ingresos en miles de pesos:");
 
 while (ingresos <= 0) {
     ingresos = prompt("Indique sus ingresos en miles de pesos:");
-}
-
-if (ingresos > 0 && ingresos <= 100) {
-    alert("Usted no tiene prestamos disponibles.")
-} else if (ingresos > 100 && ingresos <= 200) {
-    alert("Usted tiene prestamo pre aprobado de $ 500.000")
-    let cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
-    while (cuotas != 3 && cuotas != 6 && cuotas != 12) {
-        cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
-    }
-    if (cuotas == 3) {
-        alert("Pagas 3 cuotas de 180 mil pesos")
-    } else if (cuotas == 6) {
-        alert("Pagas 6 cuotas de 100 mil pesos")
-    } else if (cuotas == 12) {
-        alert("Pagas 12 cuotas de 65 mil pesos")
-    }
-} else if (ingresos > 200 && ingresos <= 300) {
-    alert("Usted tiene prestamo pre aprobado de $ 1.000.000")
-    let cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
-    while (cuotas != 3 && cuotas != 6 && cuotas != 12) {
-        cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
-    }
-    if (cuotas == 3) {
-        alert("Pagas 3 cuotas de 320 mil pesos")
-    } else if (cuotas == 6) {
-        alert("Pagas 6 cuotas de 200 mil pesos")
-    } else if (cuotas == 12) {
-        alert("Pagas 12 cuotas de 130 mil pesos")
-    }
-} else if (ingresos > 300) {
-    alert ("Consulte con un ejecutivo, por mayor monto")
 };
+
+    if (ingresos > 0 && ingresos <= 100) {
+        alert("Usted no tiene prestamos disponibles.")
+    } else if (ingresos > 100 && ingresos <= 200) {
+        alert("Usted tiene prestamo pre aprobado de $ 500.000")
+        let cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
+        while (cuotas != 3 && cuotas != 6 && cuotas != 12) {
+            cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
+        }
+        if (cuotas == 3) {
+            alert("Pagas 3 cuotas de 180 mil pesos")
+        } else if (cuotas == 6) {
+            alert("Pagas 6 cuotas de 100 mil pesos")
+        } else if (cuotas == 12) {
+            alert("Pagas 12 cuotas de 65 mil pesos")
+        }
+    } else if (ingresos > 200 && ingresos <= 300) {
+        alert("Usted tiene prestamo pre aprobado de $ 1.000.000")
+        let cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
+        while (cuotas != 3 && cuotas != 6 && cuotas != 12) {
+            cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
+        }
+        if (cuotas == 3) {
+            alert("Pagas 3 cuotas de 320 mil pesos")
+        } else if (cuotas == 6) {
+            alert("Pagas 6 cuotas de 200 mil pesos")
+        } else if (cuotas == 12) {
+            alert("Pagas 12 cuotas de 130 mil pesos")
+        }
+    } else if (ingresos > 300) {
+        alert ("Consulte con un ejecutivo, por mayor monto")
+    };
+
 
 alert("Muchas gracias por su visita!");
