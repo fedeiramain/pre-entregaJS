a24d45c132f4074da205df306a113001e339f959
fedeiramain <federicoiramain@gmail.com>
2023-03-16 08:34:24 -0300

    tienda iphone

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,77 @@
-alert("Bienvenidos a Prestamos!!");
+class producto {
+    constructor (phone, caracteristica, precio) {
+        this.phone = phone;
+        this.caracteristica = caracteristica;
+        this.precio = precio;
+    }
+}
+
+const iphone12 = new producto("Iphone12", "64GB", 150.000);
+const iphone12Pro = new producto("Iphone12Pro", "128GB", 190.000);
+const iphone13 = new producto("Iphone13", "128GB", 210.000);
+const iphone13Pro = new producto("Iphone13Pro", "256GB", 250.000);
+
+const productosIphone = [iphone12, iphone12Pro, iphone13, iphone13Pro];
+
+// console.log(productosIphone);
+
+const listaIphonePhone = productosIphone.forEach((producto) => {
+    console.log(producto.phone);
+});
+
+const listaIphonePrecio = productosIphone.forEach((producto) => {
+    console.log(producto.precio);
+});
+
+
+const iphone14 = new producto("Iphone14", "256GB", 290.000);
+const iphone14Pro = new producto("Iphone14Pro", "500GB", 340.000);
+
+
+const ingresoIphone14 = [iphone14, iphone14Pro];
+
+// console.log(ingresoIphone14);
+
+const listaContado = productosIphone.concat(ingresoIphone14);
+
+// console.log(listaContado);
+
+const masVendidos = listaContado.filter((producto) => producto.precio > 200 && producto.precio < 300);
+// console.log(masVendidos);
+
+const buscarIphone15 = listaContado.some((producto) => producto.phone === "Iphone15");
+// console.log(buscarIphone15);
+
+const preciosFinanciados = listaContado.map((producto) => {
+    return {
+         nombre: producto.nombre,
+         caracteristica: producto.caracteristica,
+         precio: producto.precio*1.05,
+    }
+});
+
+// console.log(preciosFinanciados);
+
+alert("Bienvenidos a Iphone Store");
+
+const tipoDeCompra = prompt("Indique como comprar: \n Contado \n Financiado");
+
+while (tipoDeCompra != "contado" && tipoDeCompra != "financiado") {
+    tipoDeCompra = prompt("Indique como comprar: \n Contado \n Financiado");
+};
+
+function selcLista() {
+    if(tipoDeCompra === "contado") {
+        console.log(listaContado);
+    } else {
+        console.log(preciosFinanciados);
+    }
+};
+
+selcLista();
+
+
+alert("Bienvenidos a Seccion Prestamos!!");
 
 function ingreseUsuario() {
     let usuario = prompt("Por favor, ingrese su usuario:");
@@ -61,3 +134,4 @@ function calculoCuota() {
 calculoCuota();
 
 alert("Muchas gracias por su visita!");
+
