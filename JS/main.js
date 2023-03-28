0b6f660c81dab15fc845c542f81e28f7e6ce7d54
fedeiramain <federicoiramain@gmail.com>
2023-03-28 17:32:48 -0300

    js

--- JS/main.js
+++ JS/main.js
@@ -1,137 +1,61 @@
-class producto {
-    constructor (phone, caracteristica, precio) {
-        this.phone = phone;
-        this.caracteristica = caracteristica;
-        this.precio = precio;
-    }
-}
+const productos = [
+ {nombre: "Iphone 12",
+    categoria: "iphone",
+    precio: 200000,
+    img:"./img/iphone.jpg"
+},
+{nombre: "Iphone 13",
+    categoria: "iphone",
+    precio: 280000,
+    img:"./img/iphone.jpg"
+},
+{nombre: "Iphone 14",
+    categoria: "iphone",
+    precio: 320000,
+    img:"./img/iphone.jpg"
+},
+{nombre: "Ipad Air",
+    categoria: "ipad",
+    precio: 240000,
+    img:"./img/ipad.jpg"
+},
+{nombre: "Ipad 10gen",
+    categoria: "ipad",
+    precio: 220000,
+    img:"./img/ipad.jpg"
+},
+{nombre: "Ipad Pro",
+categoria: "ipad",
+precio: 280000,
+img:"./img/ipad.jpg"
+},
+{nombre: "Airpods",
+    categoria: "accesorios",
+    precio: 45000,
+    img:"./img/airpod.jfif"
+},
+{nombre: "Earpods",
+    categoria: "accesorios",
+    precio: 8000,
+    img:"./img/erapods.jpg"
+},
+{nombre: "Cargador",
+    categoria: "accesorios",
+    precio: 6000,
+    img:"./img/cargador.jpg"
+},
+];
 
-const iphone12 = new producto("Iphone12", "64GB", 150.000);
-const iphone12Pro = new producto("Iphone12Pro", "128GB", 190.000);
-const iphone13 = new producto("Iphone13", "128GB", 210.000);
-const iphone13Pro = new producto("Iphone13Pro", "256GB", 250.000);
+const contenedorProductos = document.querySelector(".productos");
 
-const productosIphone = [iphone12, iphone12Pro, iphone13, iphone13Pro];
-
-// console.log(productosIphone);
-
-const listaIphonePhone = productosIphone.forEach((producto) => {
-    console.log(producto.phone);
-});
-
-const listaIphonePrecio = productosIphone.forEach((producto) => {
-    console.log(producto.precio);
-});
-
-
-const iphone14 = new producto("Iphone14", "256GB", 290.000);
-const iphone14Pro = new producto("Iphone14Pro", "500GB", 340.000);
-
-
-const ingresoIphone14 = [iphone14, iphone14Pro];
-
-// console.log(ingresoIphone14);
-
-const listaContado = productosIphone.concat(ingresoIphone14);
-
-// console.log(listaContado);
-
-const masVendidos = listaContado.filter((producto) => producto.precio > 200 && producto.precio < 300);
-// console.log(masVendidos);
-
-const buscarIphone15 = listaContado.some((producto) => producto.phone === "Iphone15");
-// console.log(buscarIphone15);
-
-const preciosFinanciados = listaContado.map((producto) => {
-    return {
-         nombre: producto.nombre,
-         caracteristica: producto.caracteristica,
-         precio: producto.precio*1.05,
-    }
-});
-
-// console.log(preciosFinanciados);
-
-// alert("Bienvenidos a Iphone Store");
-
-// const tipoDeCompra = prompt("Indique como comprar: \n Contado \n Financiado");
-
-// while (tipoDeCompra != "contado" && tipoDeCompra != "financiado") {
-//     tipoDeCompra = prompt("Indique como comprar: \n Contado \n Financiado");
-// };
-
-function selcLista() {
-    if(tipoDeCompra === "contado") {
-        console.log(listaContado);
-    } else {
-        console.log(preciosFinanciados);
-    }
-};
-
-selcLista();
-
-
-// alert("Bienvenidos a Seccion Prestamos!!");
-
-function ingreseUsuario() {
-    let usuario = prompt("Por favor, ingrese su usuario:");
-    while (usuario == "") {
-        usuario = prompt("Por favor, ingrese su usuario:");
-    };
-    
-    let contraseña = prompt("ingrese su clave:");
-    let autoriza = 1234;
-
-    while (contraseña != autoriza) {
-         contraseña = prompt("incorrecto, ingrese su clave:");
-    };
-};
-
-ingreseUsuario();
-
-// alert("Prestamos personales");
-
-let ingresos = prompt("Indique sus ingresos en miles de pesos:");
-
-while (ingresos <= 0) {
-    ingresos = prompt("Indique sus ingresos en miles de pesos:");
-};
-
-function calculoCuota() {
-    if (ingresos > 0 && ingresos <= 100) {
-        alert("Usted no tiene prestamos disponibles.")
-    } else if (ingresos > 100 && ingresos <= 200) {
-        alert("Usted tiene prestamo pre aprobado de $ 500.000")
-        let cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
-        while (cuotas != 3 && cuotas != 6 && cuotas != 12) {
-            cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
-        }
-        if (cuotas == 3) {
-            alert("Pagas 3 cuotas de 180 mil pesos")
-        } else if (cuotas == 6) {
-            alert("Pagas 6 cuotas de 100 mil pesos")
-        } else if (cuotas == 12) {
-            alert("Pagas 12 cuotas de 65 mil pesos")
-        }
-    } else if (ingresos > 200 && ingresos <= 300) {
-        alert("Usted tiene prestamo pre aprobado de $ 1.000.000")
-        let cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
-        while (cuotas != 3 && cuotas != 6 && cuotas != 12) {
-            cuotas = prompt("ingrese cantidad de cuotas (3, 6 y 12):")
-        }
-        if (cuotas == 3) {
-            alert("Pagas 3 cuotas de 320 mil pesos")
-        } else if (cuotas == 6) {
-            alert("Pagas 6 cuotas de 200 mil pesos")
-        } else if (cuotas == 12) {
-            alert("Pagas 12 cuotas de 130 mil pesos")
-        }
-    } else if (ingresos > 300) {
-        alert ("Consulte con un ejecutivo, por mayor monto")
-   };
-}
-
-calculoCuota();
-
-// alert("Muchas gracias por su visita!");
-
+productos.forEach(producto => {
+    let div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `<img class="img-producto" src= ${producto.img} alt="">
+                     <div class="detalle-producto">
+                         <h3 class="titulo-producto">${producto.nombre}</h3>
+                         <p>Precio: ${producto.precio}</p>
+                         <button class="agregar-carrito">Agregar</button>
+                     </div>`
+    contenedorProductos.append(div);
+})
