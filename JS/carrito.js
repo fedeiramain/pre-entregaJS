697665bd22de8b4521db3246cfca9aed10686632
fedeiramain <federicoiramain@gmail.com>
2023-03-30 17:31:35 -0300

    JS

---
+++ JS/carrito.js
@@ -0,0 +1,64 @@
+const pedidos = JSON.parse(localStorage.getItem("enCarrito"));
+const ventas = document.querySelector("#productos-carrito");
+const carritoVacio = document.querySelector(".carrito-vacio");
+const suCompra = document.querySelector(".su-compra");
+const carritoAcc = document.querySelector(".agregar-vaciar");
+
+
+function listaCompra() {
+    ventas.innerHTML = "";
+    if(pedidos) {
+        carritoVacio.classList.add("disabled");
+        pedidos.forEach(pedido => {
+            const unidad = document.createElement("div");
+            unidad.classList.add("producto-carrito");
+            unidad.innerHTML = `
+            <img class="img-producto-carrito" src=${pedido.img} alt=${pedido.nombre}>
+              <div class="titulo-producto-carrito">
+                  <small>Titulo</small>
+                  <p>${pedido.nombre}</p>
+              </div>
+              <div class="cantidad-producto-carrito">
+                  <small>Cantidad</small>
+                  <p class="producto-cantidad">${pedido.cantidad}</p>
+              </div>
+              <div class="precio-producto-carrito">
+                  <small>Precio</small>
+                  <p>$${pedido.precio}</p>
+              </div>
+              <div class="subtotal-producto-carrito">
+                  <small>Total</small>
+                  <p class="producto-total">$${pedido.precio * pedido.cantidad}</p>
+              </div>
+              <button class="btn-eliminar" id=${pedido.id}><i class="fa-solid fa-trash-can"></i></button>
+            `
+    
+            ventas.append(unidad);
+        });
+    }else {
+        carritoVacio.classList.remove("disabled");
+        suCompra.classList.add("disabled");
+        carritoAcc.classList.add("disabled");
+    }
+    eliminarproducto();
+}
+
+listaCompra();
+
+function eliminarproducto() {
+     vaciar = document.querySelectorAll(".btn-eliminar");
+     vaciar.forEach(boton => {
+        boton.addEventListener("click", quitarDeCarrito)
+     })
+
+     quitarDeCarrito(e);
+}
+
+function quitarDeCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const index = pedidos.findIndex(producto => producto.id === idBoton);
+    pedidos.splice(index, 1);
+    listaCompra();
+
+    localStorage.setItem("carrito-actualizado", JSON.stringify(pedidos));
+}
--- JS/main.js
+++ JS/main.js
@@ -1,61 +1,111 @@
 const productos = [
- {nombre: "Iphone 12",
+ {  nombre: "Iphone 12",
+    id: "iphone-12",
     categoria: "iphone",
     precio: 200000,
-    img:"./img/iphone.jpg"
+    img:"./img/iphone.jpg",
 },
-{nombre: "Iphone 13",
+{   nombre: "Iphone 13",
+    id: "iphone-13",
     categoria: "iphone",
     precio: 280000,
     img:"./img/iphone.jpg"
 },
-{nombre: "Iphone 14",
+{   nombre: "Iphone 14",
+    id: "iphone-14",
     categoria: "iphone",
     precio: 320000,
     img:"./img/iphone.jpg"
 },
-{nombre: "Ipad Air",
+{   nombre: "Ipad Air",
+    id: "ipadair",
     categoria: "ipad",
     precio: 240000,
     img:"./img/ipad.jpg"
 },
-{nombre: "Ipad 10gen",
+{   nombre: "Ipad 10gen",
+    id: "ipad10gren",
     categoria: "ipad",
     precio: 220000,
     img:"./img/ipad.jpg"
 },
-{nombre: "Ipad Pro",
-categoria: "ipad",
-precio: 280000,
-img:"./img/ipad.jpg"
+{  nombre: "Ipad Pro",
+   id: "ipadpro",
+   categoria: "ipad",
+   precio: 280000,
+   img:"./img/ipad.jpg"
 },
-{nombre: "Airpods",
+{   nombre: "Airpods",
+    id: "airpods",
     categoria: "accesorios",
     precio: 45000,
     img:"./img/airpod.jfif"
 },
-{nombre: "Earpods",
+{   nombre: "Earpods",
+    id: "earpods",
     categoria: "accesorios",
     precio: 8000,
     img:"./img/erapods.jpg"
 },
-{nombre: "Cargador",
+{   nombre: "Cargador",
+    id: "cargador",
     categoria: "accesorios",
     precio: 6000,
     img:"./img/cargador.jpg"
 },
 ];
 
-const contenedorProductos = document.querySelector(".productos");
+const contenedorProductos = document.querySelector("#productos");
 
-productos.forEach(producto => {
-    let div = document.createElement("div");
-    div.classList.add("producto");
-    div.innerHTML = `<img class="img-producto" src= ${producto.img} alt="">
-                     <div class="detalle-producto">
-                         <h3 class="titulo-producto">${producto.nombre}</h3>
-                         <p>Precio: ${producto.precio}</p>
-                         <button class="agregar-carrito">Agregar</button>
-                     </div>`
-    contenedorProductos.append(div);
-})
+
+function cargaProductos() {
+    productos.forEach(producto => {
+        let div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `<img class="img-producto" src= ${producto.img} alt="">
+                         <div class="detalle-producto">
+                             <h3 class="titulo-producto">${producto.nombre}</h3>
+                             <p>Precio: ${producto.precio}</p>
+                             <button class="agregar-carrito" id="${producto.id}">Agregar</button>
+                         </div>`
+        contenedorProductos.append(div);
+    });
+    cargarCarrito();
+};
+
+cargaProductos();
+
+function cargarCarrito() {
+    botonAgregar = document.querySelectorAll(".agregar-carrito");
+
+    botonAgregar.forEach(producto => {
+    producto.addEventListener("click", agregarCarrito);
+    })
+}
+
+let carrito;
+const pedidosLocal = JSON.parse(localStorage.getItem("carrito-actualizado"));
+if(pedidosLocal) {
+    carrito = pedidosLocal
+}else {
+    carrito = [];
+}
+
+
+function agregarCarrito(e) {
+    const idbtn = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idbtn);
+
+    if(carrito.some(producto => producto.id === idbtn)) {
+        const index = carrito.findIndex(producto => producto.id === idbtn);
+        carrito[index].cantidad +=1;
+
+    }else {
+        productoAgregado.cantidad = 1;
+        carrito.push(productoAgregado);
+
+        }
+        localStorage.setItem("enCarrito", JSON.stringify(carrito));
+    }
+   
+
